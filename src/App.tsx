0a26ac1138b9083f38ceeb0303b79020c994e82e
dyachenko.r <newxpyct@gmail.com>
2020-03-31 21:01:05 +0300

    some libraries have been added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Route, Switch, useRouteMatch } from "react-router-dom";
 import './App.css';
 
-const App = () => 
-  (
+interface IProps {};
+export const App = React.memo((props: IProps) => {
+  const matchUrl = useRouteMatch()?.url || "";
+  return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -21,6 +24,4 @@ const App = () =>
       </header>
     </div>
   );
-
-
-export default App;
+});
